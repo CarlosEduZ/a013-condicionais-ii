d22e24bace94868e38653d52c19f4b551cbe7726
Carlos Eduardo <eduardo-1942@hotmail.com>
2022-08-09 16:09:50 -0300

    Resolução atividade 013

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,17 @@
-const num = //seu numero aqui
+let numero = Number(prompt("Digite um número"))
+if(numero %2 === 0){
+    if(numero %3 === 0){
+        console.log("O número é divisivel por 2 e 3")
+    }else{
+        console.log("O número não é divisivel por 3")
+    }
+}else{
+    console.log("o número não é divisivel por 2")
+}
 
+//operador lógico
+
+if(numero%2 === 0 && numero%3 ===0){
+    console.log("O número é dividido por 2 e 3")
+}else{
+    console.log("O número não é divisivel por 2 e 3")}
